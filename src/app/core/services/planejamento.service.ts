cac0f434d0ffe775c5ade0f50f4ba631a67fba65
Kayky Bezerra <101013796+kaykymnz@users.noreply.github.com>
2026-03-20 22:08:40 -0300

    feat: add 5w2h planning section

--- src/app/core/services/planejamento.service.ts
+++ src/app/core/services/planejamento.service.ts
@@ -1,14 +1,15 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { PlanejamentoEstrategico, ObjetivoEstrategico, MatrizRisco } from '../models';
+import { PlanejamentoEstrategico, MatrizRisco, PlanoAcao5W2H } from '../models';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class PlanejamentoService {
   private apiUrl = 'http://localhost:8080/api/planejamento';
   private apiUrlMatriz = 'http://localhost:8080/api/planejamento/matriz-risco';
+  private apiUrl5w2h = 'http://localhost:8080/api/planejamento/5w2h';
 
   constructor(private http: HttpClient) {}
 
@@ -18,5 +19,9 @@ export class PlanejamentoService {
 
   getRiscos(): Observable<MatrizRisco[]> {
     return this.http.get<MatrizRisco[]>(this.apiUrlMatriz);
+  }
+
+  getPlanoAcao5W2H(): Observable<PlanoAcao5W2H[]> {
+    return this.http.get<PlanoAcao5W2H[]>(this.apiUrl5w2h);
   }
 }
